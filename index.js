c03b260a88cf933104e74b63c36cce8871ec4e57
Yarrabelli Anil Reddy <yarrabellianilreddy@gmail.com>
2026-08-27 11:38:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,153 @@
+//1.Variables are like container which are used to store values is called variables
+
+//2.The three keywords used in create variables are
+    //i,var
+    //ii,let
+    //iii,const
+
+//3.var a
+//4.let b
+//5.const c
+//6.declaration means declaring the variable
+//7.initialization means intialize a value to a variable
+
+//8.reassignment means reassign a value
+
+//9.redeclaration means dedeclare a variable
+
+//10.var allows redeclartion
+
+//11.let  and var allows reassignment
+
+//12. const
+
+//13.let age is declartion and 25 is intialization
+
+//14.100
+
+/*5.
+let number=100;
+number=200
+console.log(number)
+16.
+var a=10;
+console.log(a)
+
+17.
+var a=10;
+a=20;
+console.log(a)
+
+18.30
+
+19.
+var name="john";
+console.log(name)
+
+20.
+var price=500;
+    price=1000;
+
+console.log(price)
+
+22.
+var x=50;
+x=100;
+console.log(x)
+
+23.yes var variable can be reassigned
+24.yes var variable can be redeclared
+25.
+var name="rahul";
+    name="karthik"
+    console.log(name)
+
+26.
+let age=25;
+27.30;
+28.
+let y=88;
+y=100;
+console.log(y)
+//29.No it cannot be declared
+let y=1000;
+console.log(y)
+
+let name="john";
+let name="David"
+31,32 let city="chennai"
+    city="Salem"
+    console.log(city)
+let x=10;
+x=50;
+console.log(x)
+34.
+let salary=25000
+    salary=30000
+    console.log(salary)
+36.
+const pi=3.14
+
+
+37.
+const name=450;
+      name=1001
+console.log(name)      
+
+38.
+const age =25;
+const age =30;
+
+39.const variable must be intialized when we declare
+
+40.price is const variable value cannot change
+
+41.
+const country="India"
+
+42.
+const x=100;
+console.log(x)
+
+43.
+const pie=3.14
+pie=3.15
+
+44.let value can be changed,const value cannot be changed
+45.var can be reassigned and redeclared but const cannot be reassigned and redeclared
+
+46.
+console.log(HelloWorld);
+47.
+console.log(500)
+
+48.
+console.warn() is used to show warning symbol in the console
+49.
+console.error() is used to show an error message in the console
+
+50.
+alert() used to show a message to the user in a popup box
+prompt() used to ask the user to enter some Information
+confirm() used to ask the user for yes/No
+document.writeln() usedv to display the content on the web page
+console.log() used to display information in the browers console*/
+
+51.
+let studentName="Anil";
+let age=25;
+let mark=90;
+console.log(studentName)
+console.log(age)
+console.log(mark)
+
+let name=prompt("enter your name");
+alert(name);
+ let ages=prompt("enter your age");
+ console.log(ages)
+
+ let answer=confirm("Are you ready to learn javascript");
+ console.log(answer)
+
+ let names=prompt("Enter your name");
+ document.writeln(name);
